1d7644a68d1715bad6774ee603db86e32ae53630
ces5tg <cesar.pacho@tecsup.edu.pe>
2023-09-05 13:49:05 -0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,11 @@
+function saludar() {
+    const mensaje = '¡Hola desde el script!';
+
+    // Mostrar el mensaje en un elemento HTML con el id 'saludo'
+    const saludoElement = document.getElementById('saludo');
+    saludoElement.textContent = mensaje;
+}
+
+// Asignar la función 'saludar' al evento 'click' del botón
+const botonSaludo = document.getElementById('boton-saludo');
+botonSaludo.addEventListener('click', saludar);
